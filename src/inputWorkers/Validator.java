3273740032f433a06775586382d7bb7a96fbcd71
MKoblents <koblentsmaria@gmail.com>
2026-02-21 14:23:02 +0300

    validate methods from boolean to void

--- src/inputWorkers/Validator.java
+++ src/inputWorkers/Validator.java
@@ -2,7 +2,6 @@ package inputWorkers;
 
 import enums.MeleeWeapon;
 import exceptions.*;
-import manager.Chapter;
 import manager.CollectionManager;
 import manager.Coordinates;
 import manager.SpaceMarine;
@@ -17,91 +16,96 @@ public class Validator {
         this.spaceMarines = manager.getSpaceMarines();
         this.manager =manager;
     }
-    public boolean isSpaceMarinesValid(List<SpaceMarine> spaceMarines){
+    public void spaceMarinesValidate(List<SpaceMarine> spaceMarines){
         for (SpaceMarine spaceMarine: spaceMarines){
-            isSpaceMarineValid(spaceMarine);
-        }return true;
-    }
-
-    public boolean isSpaceMarineValid(SpaceMarine spaceMarine){
-        try {
-            return isIdValid(spaceMarine) && isNameValid(spaceMarine) && isCoordinatesValid(spaceMarine)&&isCreationDateValid(spaceMarine) && isHealthValid(spaceMarine) && isMeleeWeaponValid(spaceMarine) && isChapterValid(spaceMarine);
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-            isSpaceMarineValid(spaceMarine);
-            return true;
+            spaceMarineValidate(spaceMarine);
         }
     }
 
-    public boolean isChapterValid(SpaceMarine spaceMarine) throws NullPointerException {
-        return isChapterNameValid(spaceMarine)&&isChapterWorldValid(spaceMarine);
+    public void spaceMarineValidate(SpaceMarine spaceMarine){
+        try {
+            idValidate(spaceMarine);
+            nameValidate(spaceMarine);
+            coordinatesValidate(spaceMarine);
+            creationDateValidate(spaceMarine);
+            healthValidate(spaceMarine);
+            meleeWeaponValidate(spaceMarine);
+            chaterValidate(spaceMarine);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            spaceMarineValidate(spaceMarine);
+        }
     }
-    public boolean isChapterNameValid(SpaceMarine spaceMarine) throws NullPointerException{
+
+    public void chaterValidate(SpaceMarine spaceMarine) throws NullPointerException {
+         chapterNameValidate(spaceMarine);
+         chapterWorldValidate(spaceMarine);
+    }
+    public void chapterNameValidate(SpaceMarine spaceMarine) throws NullPointerException{
         String name = spaceMarine.getChapter().getName();
         if (name == null || name.isEmpty()) {
             spaceMarine.getChapter().setName("Chapter"+ spaceMarine.getId());
             throw new NullPointerException("SpaceMarine name can't be null or empty!. Replaced with Chapter" +spaceMarine.getId());
-        }return true;
+        }
     }
-    public boolean isChapterWorldValid(SpaceMarine spaceMarine) throws NullPointerException{
+    public void chapterWorldValidate(SpaceMarine spaceMarine) throws NullPointerException{
         String world = spaceMarine.getChapter().getWorld();
         if (world == null || world.isEmpty()) {
             spaceMarine.getChapter().setWorld("ChapteWorldr"+ spaceMarine.getId());
             throw new NullPointerException("SpaceMarine world can't be null or empty!. Replaced with ChapterWorld" +spaceMarine.getId());
-        }return true;
+        };
     }
-    public boolean isMeleeWeaponValid(SpaceMarine spaceMarine) throws  UnavailableMeleeWeaponException{
+    public void meleeWeaponValidate(SpaceMarine spaceMarine) throws  UnavailableMeleeWeaponException{
         if (spaceMarine.getMeleeWeapon() == null){
             spaceMarine.setMeleeWeapon(MeleeWeapon.CHAIN_AXE);
             throw new UnavailableMeleeWeaponException(spaceMarine.getName(),MeleeWeapon.CHAIN_AXE);
-        }return true;
+        }
     }
-    public boolean isCreationDateValid(SpaceMarine spaceMarine) throws CreationDataException{
+    public void creationDateValidate(SpaceMarine spaceMarine) throws CreationDataException{
         ZonedDateTime originalTime = spaceMarine.getCreationDate();
         if (originalTime == null || originalTime.toInstant().toEpochMilli() <=0){
             ZonedDateTime correctTime = ZonedDateTime.now();
             spaceMarine.setCreationDate(correctTime);
             throw new CreationDataException(spaceMarine.getName(),originalTime, correctTime);
-        }return true;
+        }
     }
-    public boolean isIdValid(SpaceMarine spaceMarine) throws WrongIdException{
+    public void idValidate(SpaceMarine spaceMarine) throws WrongIdException{
         long correctId = manager.getCorrectId(spaceMarine);
         long realId = spaceMarine.getId();
-        if ( correctId== realId){
-            return true;
-        }else {
+        if ( correctId != realId){
             spaceMarine.setId(correctId);
             throw new WrongIdException(spaceMarine.getName(), realId, correctId);
         }
     }
-    public boolean isNameValid(SpaceMarine spaceMarine) throws NullPointerException{
+    public void nameValidate(SpaceMarine spaceMarine) throws NullPointerException{
         String name = spaceMarine.getName();
         if (name == null || name.isEmpty()){
             spaceMarine.setName("SpaceMarine"+spaceMarine.getId());
             throw new NullPointerException("SpaceMarine name can't be null or empty!. Replaced with SpaceMarine"+spaceMarine.getId());
-        }return true;
+        }
     }
-    public boolean isCoordinatesValid(SpaceMarine spaceMarine) throws UnavailableCoordinateException {
+    public void coordinatesValidate(SpaceMarine spaceMarine) throws UnavailableCoordinateException {
         Coordinates coordinates = spaceMarine.getCoordinates();
-        return isXCoordinateValid(coordinates) && isYCoordinateValid(coordinates);
+        xCoordinateValidate(coordinates);
+        yCoordinateValidate(coordinates);
     }
-    public boolean isXCoordinateValid(Coordinates coordinates) throws UnavailableCoordinateException {
-        if (coordinates.getX()<-617){//TODO diapason
+    public void xCoordinateValidate(Coordinates coordinates) throws UnavailableCoordinateException {
+        if (coordinates.getX()<-617){
             coordinates.setX(0);
             throw new UnavailableCoordinateException("X must be bigger than -617, replaced with 0");
-        }return true;
+        }
     }
-    public boolean isYCoordinateValid(Coordinates coordinates) throws UnavailableCoordinateException {
+    public void yCoordinateValidate(Coordinates coordinates) throws UnavailableCoordinateException {
         if (coordinates.getY()<-842){
             throw new UnavailableCoordinateException("Y must be bigger than -842, replaced with 0");
-        }return true;
+        }
     }
-    public boolean isHealthValid(SpaceMarine spaceMarine) throws UnavailableHealthException{
+    public void healthValidate(SpaceMarine spaceMarine) throws UnavailableHealthException{
         double health = spaceMarine.getHealth();
         if (health <= 0.0){
             spaceMarine.setHealth(0.0);
             throw new UnavailableHealthException(spaceMarine.getName(),health,0.0);
-        }return true;
+        }
     }
 
 }
