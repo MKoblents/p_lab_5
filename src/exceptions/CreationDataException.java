aa49ed02802b2d6cbf145e5b869eaa14a39849d3
MKoblents <koblentsmaria@gmail.com>
2026-02-21 13:55:18 +0300

    corrected SpaceMarine validation. RunTimeException->Exception

--- src/exceptions/CreationDataException.java
+++ src/exceptions/CreationDataException.java
@@ -2,7 +2,7 @@ package exceptions;
 
 import java.time.ZonedDateTime;
 
-public class CreationDataException extends RuntimeException {
+public class CreationDataException extends Exception {
     public CreationDataException(String name, ZonedDateTime wrongTime, ZonedDateTime correctTime) {
         super("SpaceMarine "+name+" had wrong creation data "+wrongTime+" replaced with "+correctTime);
     }
--- src/exceptions/UnavailableHealthException.java
+++ src/exceptions/UnavailableHealthException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class UnavailableHealthException extends RuntimeException {
+public class UnavailableHealthException extends Exception {
     public UnavailableHealthException(String name, double originalHealth, double newHealth) {
         super("SpaceMarine "+name+" had wrong health "+originalHealth+" replaced with "+newHealth);
     }
