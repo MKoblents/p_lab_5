748973ca405ef216214d9d7210523d0d1661dfc5
MKoblents <koblentsmaria@gmail.com>
2026-02-21 16:58:39 +0300

    clear command

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import commands.ClearCommand;
 import commands.HelpCommand;
 import commands.InfoCommand;
 import commands.ShowCommand;
@@ -19,8 +20,10 @@ public class Main {
             invoker.registerCommand("help", new HelpCommand(invoker));
             invoker.registerCommand("info", new InfoCommand(collectionManager));
             invoker.registerCommand("show", new ShowCommand(collectionManager));
+            invoker.registerCommand("clear", new ClearCommand(collectionManager));
             invoker.runCommand("help");
             invoker.runCommand("info");
+            //invoker.runCommand("clear");
             invoker.runCommand("show");
 //            String xmlFilePath = "/home/mkoblents/Yandex.Disk/maria/ITMO/progaaaaaaa/p_lab_5/src/sm.xml";
 //
