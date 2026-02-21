3e968a051bf4dee165738b0a4f6328cf1601683a
MKoblents <koblentsmaria@gmail.com>
2026-02-21 13:49:11 +0300

    corrected Chapter validation. removed static.

--- src/inputWorkers/Validator.java
+++ src/inputWorkers/Validator.java
@@ -2,6 +2,7 @@ package inputWorkers;
 
 import enums.MeleeWeapon;
 import exceptions.*;
+import manager.Chapter;
 import manager.CollectionManager;
 import manager.Coordinates;
 import manager.SpaceMarine;
@@ -35,13 +36,29 @@ public class Validator {
         }
     }
 
-    public boolean isMeleeWeaponValid(SpaceMarine spaceMarine) {
+    public boolean isChapterValid(SpaceMarine spaceMarine) throws NullPointerException {
+        return isChapterNameValid(spaceMarine)&&isChapterWorldValid(spaceMarine);
+    }
+    public boolean isChapterNameValid(SpaceMarine spaceMarine) throws NullPointerException{
+        String name = spaceMarine.getChapter().getName();
+        if (name == null || name.isEmpty()) {
+            spaceMarine.getChapter().setName("Chapter"+ spaceMarine.getId());
+            throw new NullPointerException("SpaceMarine name can't be null or empty!. Replaced with Chapter" +spaceMarine.getId());
+        }return true;
+    }
+    public boolean isChapterWorldValid(SpaceMarine spaceMarine) throws NullPointerException{
+        String world = spaceMarine.getChapter().getWorld();
+        if (world == null || world.isEmpty()) {
+            spaceMarine.getChapter().setWorld("ChapteWorldr"+ spaceMarine.getId());
+            throw new NullPointerException("SpaceMarine world can't be null or empty!. Replaced with ChapterWorld" +spaceMarine.getId());
+        }return true;
+    }
+    public boolean isMeleeWeaponValid(SpaceMarine spaceMarine) throws  UnavailableMeleeWeaponException{
         if (spaceMarine.getMeleeWeapon() == null){
             spaceMarine.setMeleeWeapon(MeleeWeapon.CHAIN_AXE);
             throw new UnavailableMeleeWeaponException(spaceMarine.getName(),MeleeWeapon.CHAIN_AXE);
         }return true;
     }
-
     public boolean isCreationDateValid(SpaceMarine spaceMarine) throws CreationDataException{
         ZonedDateTime originalTime = spaceMarine.getCreationDate();
         if (originalTime == null || originalTime.toInstant().toEpochMilli() <=0){
@@ -50,7 +67,6 @@ public class Validator {
             throw new CreationDataException(spaceMarine.getName(),originalTime, correctTime);
         }return true;
     }
-
     public boolean isIdValid(SpaceMarine spaceMarine) throws WrongIdException{
         long correctId = manager.getCorrectId(spaceMarine);
         long realId = spaceMarine.getId();
@@ -65,25 +81,25 @@ public class Validator {
         String name = spaceMarine.getName();
         if (name == null || name.isEmpty()){
             spaceMarine.setName("SpaceMarine"+spaceMarine.getId());
-            throw new NullPointerException("Name can't be null or empty!. Replaced with 'SpaceMarine'"+spaceMarine.getId());
+            throw new NullPointerException("SpaceMarine name can't be null or empty!. Replaced with SpaceMarine"+spaceMarine.getId());
         }return true;
     }
-    public static boolean isCoordinatesValid(SpaceMarine spaceMarine) throws UnavailableCoordinateException {
+    public boolean isCoordinatesValid(SpaceMarine spaceMarine) throws UnavailableCoordinateException {
         Coordinates coordinates = spaceMarine.getCoordinates();
         return isXCoordinateValid(coordinates) && isYCoordinateValid(coordinates);
     }
-    public static boolean isXCoordinateValid(Coordinates coordinates) throws UnavailableCoordinateException {
+    public boolean isXCoordinateValid(Coordinates coordinates) throws UnavailableCoordinateException {
         if (coordinates.getX()<-617){//TODO diapason
             coordinates.setX(0);
             throw new UnavailableCoordinateException("X must be bigger than -617, replaced with 0");
         }return true;
     }
-    public static boolean isYCoordinateValid(Coordinates coordinates) throws UnavailableCoordinateException {
+    public boolean isYCoordinateValid(Coordinates coordinates) throws UnavailableCoordinateException {
         if (coordinates.getY()<-842){
             throw new UnavailableCoordinateException("Y must be bigger than -842, replaced with 0");
         }return true;
     }
-    public static boolean isHealthValid(SpaceMarine spaceMarine) throws UnavailableHealthException{
+    public boolean isHealthValid(SpaceMarine spaceMarine) throws UnavailableHealthException{
         double health = spaceMarine.getHealth();
         if (health <= 0.0){
             spaceMarine.setHealth(0.0);
