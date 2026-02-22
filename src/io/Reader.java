a871cfd038a38bda3d52d12217af50ec8a2e3dfd
MKoblents <koblentsmaria@gmail.com>
2026-02-22 18:11:47 +0300

    refactoring input

--- src/io/Reader.java
+++ src/io/Reader.java
@@ -1,8 +1,20 @@
 package io;
 
+import enums.MeleeWeapon;
+import manager.Chapter;
+import manager.SpaceMarine;
+
 import java.io.IOException;
 
 public interface Reader {
     String nextLine() throws IOException;
     boolean hasNextLine() throws IOException;
+    SpaceMarine getInputSpaceMarine(SpaceMarine spaceMarine);
+    String getInputString() throws IOException;
+    String getTrimmedText() throws IOException;
+    <T extends Enum<T>> T getInputEnum(Class<T> enumType) throws IOException;
+    double getInputDouble() throws IOException;
+    Chapter getInputChapter() throws IOException;
+    public long getInputLong() throws IOException;
+    public MeleeWeapon getInputMeleeWeapon();
 }
