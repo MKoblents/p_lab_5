84f163011150a85073f90e533acbb2af994f1e82
MKoblents <koblentsmaria@gmail.com>
2026-02-22 15:46:30 +0300

    save command

--- src/manager/SpaceMarine.java
+++ src/manager/SpaceMarine.java
@@ -4,17 +4,31 @@ import enums.AstartesCategory;
 import enums.MeleeWeapon;
 import enums.Weapon;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.time.ZonedDateTime;
-
+@XmlRootElement(name = "spaceMarine")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class SpaceMarine implements Comparable<SpaceMarine> {
+    @XmlElement
     private long id =1; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    @XmlElement
     private String name; //Поле не может быть null, Строка не может быть пустой
+    @XmlElement
     private Coordinates coordinates; //Поле не может быть null
+    @XmlElement
     private java.time.ZonedDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    @XmlElement
     private Double health = 0.0; //Поле может быть null, Значение поля должно быть больше 0
+    @XmlElement
     private AstartesCategory category; //Поле может быть null
+    @XmlElement
     private Weapon weaponType; //Поле может быть null
+    @XmlElement
     private MeleeWeapon meleeWeapon; //Поле не может быть null
+    @XmlElement
     private Chapter chapter; //Поле может быть null
     protected SpaceMarine(){
         //TODO id, creation data,...
