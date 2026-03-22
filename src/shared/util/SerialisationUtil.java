96baaa55eea449bc62d5aabc16540f7157d0b3ad
MKoblents <koblentsmaria@gmail.com>
2026-03-22 13:23:26 +0300

    added serialisation util

---
+++ src/shared/util/SerialisationUtil.java
@@ -0,0 +1,20 @@
+package shared.util;
+
+import java.io.*;
+
+public class SerialisationUtil {
+    public static byte[] serialize(Object object) throws IOException{
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)){
+            objectOutputStream.writeObject(object);
+            objectOutputStream.flush();
+        }
+        return byteArrayOutputStream.toByteArray();
+    }
+    public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException{
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes)){
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            return objectInputStream.readObject();
+        }
+    }
+}
