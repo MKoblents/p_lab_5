8dda5732c2e69c8a8692c835829a5a83708c6de8
MKoblents <koblentsmaria@gmail.com>
2026-03-28 17:18:34 +0300

    connectedClient init

---
+++ src/server/client/ConnectedClient.java
@@ -0,0 +1,31 @@
+package server.client;
+
+import shared.dto.ClientStatus;
+import shared.enums.ClientState;
+
+import java.time.Instant;
+import java.util.Optional;
+
+public class ConnectedClient {
+    private final String clientId;
+    private ClientState clientState;
+    private Instant lastHeartbeat;
+    private int commandsExecuted;
+    public ConnectedClient(String clientId, ClientState clientState){
+        this.clientId = clientId;
+        this.clientState = clientState;
+        this.lastHeartbeat = Instant.now();
+    }
+    public void upgradeHeartbeat(){
+        this.lastHeartbeat = Instant.now();
+    }
+    public ClientStatus getClientStatus(){
+        return new ClientStatus(clientId,clientState,lastHeartbeat,commandsExecuted, Optional.empty());
+    }
+    public void markOffline(){
+        this.clientState = ClientState.OFFLINE;
+    }
+    public void markOnline(){
+        this.clientState = ClientState.ONLINE;
+    }
+}
