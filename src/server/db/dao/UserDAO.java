e3aa4a8782276952421ef2a62c2f5919560d1901
MKoblents <koblentsmaria@gmail.com>
2026-04-28 19:55:26 +0300

    auth service added

--- src/server/db/dao/UserDAO.java
+++ src/server/db/dao/UserDAO.java
@@ -2,14 +2,10 @@ package server.db.dao;
 
 import server.db.provider.DbProvider;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Base64;
 
 public class UserDAO {
     private final DbProvider provider;
@@ -17,12 +13,11 @@ public class UserDAO {
         this.provider = provider;
     }
     public void register(String user, String password) throws SQLException {
-        String hashPassword = hashPassword(password);
         String sql = "insert into Users (name, password) values (?,?);";
         try (Connection connection = provider.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-            preparedStatement.setString(1, password);
-            preparedStatement.setString(2,password);
+            preparedStatement.setString(1, user);
+            preparedStatement.setString(2, password);
             preparedStatement.executeUpdate();
         }
     }
@@ -37,15 +32,6 @@ public class UserDAO {
                 return null;
 
             }
-        }
-    }
-    private String hashPassword(String password){
-        try{
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-384");
-            byte[] digest = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
-            return Base64.getEncoder().encodeToString(digest);
-        }catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("SHA-384 algorithm is not available", e);
         }
     }
 }
